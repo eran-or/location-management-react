d03f8f2d73bcd839790a1ec27308e006f897d32c
eran-or <e.or1976@gmail.com>
2017-12-03 10:56:40 +0200

    Updates

--- src/selectors/categories.js
+++
@@ -1,7 +0,0 @@
-export default (categories, { sortCategoriesOrder }) => {
-  let copy = [...categories]
-   copy.sort((a, b) => {
-    return ('Alphabetically' === sortCategoriesOrder) ? (a.name > b.name ? 1 : -1) : '';
-  })
-  return copy;
-}
